1e4bd9ab3790beac180a3ec96976cafa046647ae
maxrt <max.r.tkachuk@gmail.com>
2026-02-19 02:41:51 +0200

    Add macros for named tasks

--- include/async/macros.h
+++ include/async/macros.h
@@ -18,6 +18,20 @@
   void __fn ## _worker (__VA_ARGS__)
 
 
+#define async_task_n(__fn, __name, ...)                           \
+  void __fn ## _worker (__VA_ARGS__);                             \
+  std::shared_ptr<async::Future<void>> __fn(auto... args) {       \
+    auto f = std::make_shared<async::Future<void>>();             \
+    auto loop = async::getGlobalLoop();                           \
+    loop->addTask([f, args...]() {                                \
+      __fn ## _worker(args...); f->complete();                    \
+    }, __name);                                                   \
+    return f;                                                     \
+  }                                                               \
+  void __fn ## _worker (__VA_ARGS__)
+
+
+
 #define async_task_r_decl(__fn, __ret) \
   std::shared_ptr<async::Future<__ret>> __fn(auto... args);
 
@@ -33,6 +47,20 @@
     return f;                                                     \
   }                                                               \
   __ret __fn ## _worker (__VA_ARGS__)
+
+
+#define async_task_n_r(__fn, __name, __ret, ...)                  \
+  __ret __fn ## _worker (__VA_ARGS__);                            \
+  std::shared_ptr<async::Future<__ret>> __fn(auto... args) {      \
+    auto f = std::make_shared<async::Future<__ret>>();            \
+    auto loop = async::getGlobalLoop();                           \
+    loop->addTask([f, args...]() {                                \
+      f->complete(__fn ## _worker(args...));                      \
+    }, __name);                                                   \
+    return f;                                                     \
+  }                                                               \
+  __ret __fn ## _worker (__VA_ARGS__)
+
 
 
 #define async_main()                                              \
