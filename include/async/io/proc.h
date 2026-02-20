41844c83c718ee5c4750b4d627b24e057341a0a5
maxrt <max.r.tkachuk@gmail.com>
2026-02-20 02:58:59 +0200

    Rewrite Process to support dynamic io

--- include/async/io/proc.h
+++ include/async/io/proc.h
@@ -20,13 +20,28 @@ struct ProcessNotRunningException final : std::runtime_error {
 
 class Process {
 public:
+  struct IO {
+    std::shared_ptr<File> in  = std::make_shared<File>();
+    std::shared_ptr<File> out = std::make_shared<File>();
+    std::shared_ptr<File> err = std::make_shared<File>();
+  };
+
+  using IORef = std::shared_ptr<IO>;
+
   struct Result {
     bool ok = false;
 
     int exit_code = 0;
 
-    std::shared_ptr<File> out = std::make_shared<File>();
-    std::shared_ptr<File> err = std::make_shared<File>();
+    std::shared_ptr<IO> io = std::make_shared<IO>();
+  };
+
+  using ResultRef    = std::shared_ptr<Result>;
+  using ResultFuture = std::shared_ptr<Future<ResultRef>>;
+
+  struct Started {
+    ResultFuture future;
+    IORef        io;
   };
 
 private:
@@ -41,36 +56,118 @@ private:
     int in[2];
     int out[2];
     int err[2];
+    int service[2];
 
-    void close() {
-      for (int p : {in[0], in[1], out[0], out[1], err[0], err[1]}) {
-        if (p) ::close(p);
+    const std::string& input;
+
+    explicit Pipes(const std::string& input) : input(input) {
+      pipe(out);
+      pipe(err);
+      pipe(in);
+
+      pipe(service);
+      fcntl(service[0], F_SETFL, fcntl(service[0], F_GETFL) | O_NONBLOCK);
+    }
+
+    ~Pipes() {
+      closeFromParent();
+    }
+
+    static std::shared_ptr<Pipes> create(const std::string& input) {
+      return std::make_shared<Pipes>(input);
+    }
+
+    static void close(int& fd) {
+      if (fd) {
+        ::close(fd);
+        fd = 0;
       }
     }
+
+    void prepareChild() {
+      dup2(out[1], STDOUT_FILENO);
+      dup2(err[1], STDERR_FILENO);
+      dup2(in[0], STDIN_FILENO);
+
+      closeFromChild();
+    }
+
+    void prepareParent() {
+      close(out[1]);
+      close(err[1]);
+      close(in[0]);
+
+      if (!input.empty()) {
+        write(in[1], input.c_str(), input.size());
+        close(in[1]);
+      }
+    }
+
+    bool hadMarker() const {
+      uint8_t marker = 0;
+
+      if (read(service[0], &marker, 1) == 1) {
+        return marker == '1';
+      }
+
+      return false;
+    }
+
+    void closeFromChild() {
+      // Close all, except for write end of service pipe
+      close(in[0]);
+      close(in[1]);
+      close(out[0]);
+      close(out[1]);
+      close(err[0]);
+      close(err[1]);
+      close(service[0]);
+    }
+
+    void closeFromParent() {
+      // Close all, except for the ones used in IO struct
+      // those descriptors will be owned by File, and will
+      // get closed once the user discards Result/Process
+      close(in[0]);
+      close(out[1]);
+      close(err[1]);
+      close(service[0]);
+      close(service[1]);
+    }
   };
+
+  using PipesRef = std::shared_ptr<Pipes>;
 
   State       state;
   pid_t       pid;
   std::mutex  mutex;
   std::string command;
   std::string input;
-  Result      result;
+  ResultRef   result;
 
 public:
-  Process() : state(State::NONE), pid(0) {}
+  Process()
+    : state(State::NONE),
+      pid(0),
+      result(std::make_shared<Result>()) {}
 
-  explicit Process(const std::string& command) : state(State::NONE), pid(0), command(command) {}
+  explicit Process(const std::string& command)
+    : state(State::NONE),
+      pid(0),
+      command(command),
+      result(std::make_shared<Result>()) {}
 
-  Process(const std::string& command, const std::string& input) : state(State::NONE), pid(0), command(command), input(input), result() {}
+  Process(const std::string& command, const std::string& input)
+    : state(State::NONE),
+      pid(0),
+      command(command),
+      input(input),
+      result(std::make_shared<Result>()) {}
 
   ~Process() {}
 
   static std::shared_ptr<Process> create(const std::string& command, const std::string& input = "") {
     return std::make_shared<Process>(command, input);
-  }
-
-  static std::shared_ptr<Future<Result>> run(const std::string& command, const std::string& input = "") {
-    return create(command, input)->start();
   }
 
   bool isRunning() {
@@ -79,13 +176,34 @@ public:
     return state == State::EXEC;
   }
 
-  std::shared_ptr<Future<Result>> start() {
+  Started start() {
     auto lock = std::unique_lock(mutex);
 
     state = State::INIT;
 
-    return async::task<Result>([this] {
-      this->execute();
+    auto pipes = Pipes::create(input);
+    this->execute(pipes);
+
+    return {
+      async::task<ResultRef>([this, pipes] {
+        this->wait(pipes);
+        return this->result;
+      }),
+      result->io
+    };
+  }
+
+  ResultFuture run() {
+    auto lock = std::unique_lock(mutex);
+
+    state = State::INIT;
+
+    return async::task<ResultRef>([this] {
+      const auto pipes = Pipes::create(input);
+
+      this->execute(pipes);
+      this->wait(pipes);
+
       return this->result;
     });
   }
@@ -98,77 +216,73 @@ public:
     ::kill(pid, sig);
   }
 
-  static std::shared_ptr<Future<Result>> chain(const std::shared_ptr<Process>& proc1, const std::shared_ptr<Process>& proc2) {
-    const auto r = proc1->start()->await();
-
-    proc2->input = r.out->readAll()->await();
-
-    return proc2->start();
+  static ResultFuture chain(
+    const std::shared_ptr<Process>& proc1,
+    const std::shared_ptr<Process>& proc2
+  ) {
+    const auto r = proc1->await();
+  
+    proc2->input = r->io->out->readAll()->await();
+  
+    return proc2->run();
   }
 
-  Result await() {
-    return start()->await();
+  ResultRef await() {
+    return run()->await();
   }
 
 private:
-  void execute() {
-    const auto pipes = new Pipes {0};
-
-    pipe(pipes->out);
-    pipe(pipes->err);
-
-    if (!input.empty()) {
-      pipe(pipes->in);
-    }
-
+  void execute(PipesRef pipes) {
     pid = fork();
 
     if (pid == 0) {
-      dup2(pipes->out[1], STDOUT_FILENO);
-      dup2(pipes->err[1], STDERR_FILENO);
+      pipes->prepareChild();
 
-      if (!input.empty()) {
-        dup2(pipes->in[0], STDIN_FILENO);
-      }
-
-      pipes->close();
       const auto argv = createArgv();
       execvp(argv[0], argv);
+
+      write(pipes->service[1], "1", 1);
+
       ::exit(-1);
     }
 
-    result.ok = true;
+    result->ok = true;
 
     if (pid < 0) {
       auto lock = std::unique_lock(mutex);
       state = State::DONE;
-      result.ok = false;
+      result->ok = false;
       return;
     }
 
-    close(pipes->out[1]);
-    close(pipes->err[1]);
+    pipes->prepareParent();
 
-    if (!input.empty()) {
-      close(pipes->in[0]);
-
-      write(pipes->in[1], input.c_str(), input.size());
+    if (pipes->in[1]) {
+      result->io->in = File::fd(pipes->in[1], "w");
     }
 
+    result->io->out = File::fd(pipes->out[0], "r");
+    result->io->err = File::fd(pipes->err[0], "r");
+
+    state = State::EXEC;
+  }
+
+  void wait(PipesRef pipes) {
     int status = 0;
 
-    if (waitpid(pid, &status, 0) == -1) {
-      result.ok = false;
+    int res = waitpid(pid, &status, 0);
+
+    auto lock = std::unique_lock(mutex);
+
+    if (res == -1) {
+      result->ok = false;
     }
+
+    result->ok = !pipes->hadMarker();
 
     if (WIFEXITED(status)) {
-      result.exit_code = WEXITSTATUS(status);
+      result->exit_code = WEXITSTATUS(status);
     }
-
-    result.out = File::fd(pipes->out[0], "r");
-    result.err = File::fd(pipes->err[0], "r");
-
-    delete pipes;
   }
 
   char ** createArgv() const {
