bc78cba618673d1e8a4f4c1c7da30aa6215d9657
maxrt <max.r.tkachuk@gmail.com>
2026-02-20 03:38:02 +0200

    Fixes for g++

--- include/async/platform/x86_64.h
+++ include/async/platform/x86_64.h
@@ -3,12 +3,12 @@
 namespace async::platform {
 
 __attribute__((always_inline))
-static void set_stack(void * stack) {
+inline static void set_stack(void * stack) {
   __asm__ volatile ("mov %0, %%rsp\n" :: "r" (stack) : "sp");
 }
 
 __attribute__((always_inline))
-static void * get_stack() {
+inline static void * get_stack() {
   void * stack;
   __asm__ volatile ("mov %%rsp, %0\n" : "=r" (stack) :: "sp");
   return stack;
