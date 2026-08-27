fcdfdc5b86ff831cc397e9b5933998cac7ab4146
maxrt <max.r.tkachuk@gmail.com>
2026-08-27 22:28:19 +0300

    Add StackPool

--- include/async/task.h
+++ include/async/task.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include <functional>
 #include <string>
 #include <cstdint>
@@ -20,6 +21,79 @@ using TaskId = int64_t;
 
 /** @brief Invalid Task ID */
 constexpr TaskId INVALID_TASK_ID = -1UL;
+
+/**
+ * @brief Singleton that manges allocated stacks, reusing them instead of
+ *        freeing and re-allocating each time
+ */
+class StackPool {
+  struct {
+    std::unordered_map<uint8_t *, size_t> free;
+    std::unordered_map<uint8_t *, size_t> used;
+  } stacks;
+
+  static inline std::shared_ptr<StackPool> instance;
+
+public:
+  StackPool() = default;
+
+  /**
+   * @brief Returns memory to the OS
+   */
+  ~StackPool() {
+    for (const auto & map : {stacks.free, stacks.used}) {
+      for (const auto& item : map) {
+        delete[] item.first;
+      }
+    }
+
+    stacks.free.clear();
+    stacks.used.clear();
+  }
+
+  /** @brief Retrieve (and lazy-initialize if needed) StackPool instance */
+  static std::shared_ptr<StackPool> get() {
+    if (!instance) {
+      instance = std::make_shared<StackPool>();
+    }
+
+    return instance;
+  }
+
+  /**
+   * @brief Allocation request. Will search `stacks.free` before resorting to
+   *        allocating new memory
+   */
+  uint8_t * alloc(const size_t size) {
+    uint8_t * ptr = nullptr;
+
+    for (const auto& item : stacks.free) {
+      if (item.second == size) {
+        ptr = item.first;
+        stacks.free.erase(ptr);
+        break;
+      }
+    }
+
+    if (!ptr) {
+      ptr = new uint8_t[size];
+    }
+
+    stacks.used[ptr] = size;
+
+    return ptr;
+  }
+
+  /**
+   * @brief Move allocated `ptr` from `stacks.used` to `stacks.free`. Won't
+   *        return memory to the system
+   */
+  void free(uint8_t * ptr) {
+    const auto size = stacks.used[ptr];
+    stacks.used.erase(ptr);
+    stacks.free[ptr] = size;
+  }
+};
 
 /**
  * @brief Task Control Block (or Task Context) for an asynchronous task
@@ -60,10 +134,10 @@ public:
       name(name),
       worker(std::move(worker)),
       ctx(),
-      stack({.data = new uint8_t[stack_size], .size = stack_size}) {}
+      stack({.data = StackPool::get()->alloc(stack_size), .size = stack_size}) {}
 
   ~Task() {
-    delete[] stack.data;
+    StackPool::get()->free(stack.data);
     stack.data = nullptr;
     stack.size = 0;
   }
