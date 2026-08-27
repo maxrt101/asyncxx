d20b9c57bc13d49ad585c128da41def2036d34c6
maxrt <max.r.tkachuk@gmail.com>
2026-08-27 19:37:06 +0300

    Document test.h; add TEST_ASSERT_ONE_OF; refactor assert macros

--- src/test.h
+++ src/test.h
@@ -7,6 +7,12 @@
 #include <string>
 #include <async/util.h>
 
+/**
+ * @brief ANSI Escape codes + BG/FG colors
+ * @defgroup ansi ANSI escape codes
+ * @{
+ */
+
 #define ANSI_TEXT_RESET       "\x1b[0m"
 #define ANSI_TEXT_BOLD        "\x1b[1m"
 
@@ -30,57 +36,170 @@
 #define ANSI_COLOR_BG_WHITE   "\x1b[47m"
 #define ANSI_COLOR_BG_DEFAULT "\x1b[49m"
 
-#define TEST_LOC_FMT                                                          \
-  ANSI_COLOR_FG_CYAN "{}" ANSI_TEXT_RESET ":"                                 \
+/** @} */
+
+/**
+ * @brief std::format-compatible (colored) format string for FILE:LINE
+ *
+ * @warning For internal use
+ */
+#define TEST_LOC_FMT                                                           \
+  ANSI_COLOR_FG_CYAN "{}" ANSI_TEXT_RESET ":"                                  \
   ANSI_COLOR_FG_MAGENTA "{}" ANSI_TEXT_RESET
 
+/**
+ * @brief Shortcut to get test function from test name
+ */
 #define TEST_FN_NAME(__name) __test_ ## __name
 
-#define TEST(__suite, __name, __desc)                                         \
-  void TEST_FN_NAME(__name) (test::TestSuite * suite);                        \
-  static const int __test_dummy_ ## __name = test::impl::registerTest(        \
-      &__suite, TEST_FN_NAME(__name), #__name, __desc);                       \
+/**
+ * @brief Declare a test
+ *
+ * @code{.c}
+ *   auto test_suite = test::TestSuite("test suite");
+ *
+ *   TEST(test_suite, test_name, "Some test that tests something") {
+ *      TEST_ASSERT(true, "Should never fail");
+ *   }
+ * @endcode
+ *
+ * @param __suite Test Suite name
+ * @param __name  Test name
+ * @param __desc  Test description (string literal)
+ */
+#define TEST(__suite, __name, __desc)                                          \
+  void TEST_FN_NAME(__name) (test::TestSuite * suite);                         \
+  static const int __test_dummy_ ## __name = test::impl::registerTest(         \
+      &__suite, TEST_FN_NAME(__name), #__name, __desc);                        \
   void TEST_FN_NAME(__name) (test::TestSuite * suite)
 
-#define TEST_ASSERT(__expr, __msg_fmt, ...)                                   \
-  do {                                                                        \
-    if (!(__expr)) {                                                          \
-      throw test::AssertionError(                                             \
-        __FILE__, __LINE__,                                                   \
-        #__expr,                                                              \
-        std::format(__msg_fmt, ## __VA_ARGS__)                                \
-      );                                                                      \
-    }                                                                         \
+/**
+ * @brief Expands to an equality assertion error common arguments
+ *
+ * @warning For internal use
+ *
+ * @param __lhs     LHS of a comparison
+ * @param __rhs     RHS of a comparison
+ * @param __msg_fmt std::format-compatible string for assertion failure message
+ * @param ...       Assertion failure message args
+ */
+#define __ASSERTION_EQ_ERROR_COMMON_ARGS(__lhs, __rhs, __msg_fmt, ...)         \
+    __FILE__, __LINE__,                                                        \
+    test::impl::to_string(__lhs),                                              \
+    test::impl::to_string(__rhs),                                              \
+    std::format(__msg_fmt, ## __VA_ARGS__)
+
+/**
+ * @brief Constructs & throws AssertionEqualityError
+ *
+ * @warning For internal use
+ *
+ * @param __op @c true - equals; @c false - not-equals
+ * For the rest of the arguments see @ref __ASSERTION_EQ_ERROR_COMMON_ARGS
+ */
+#define __THROW_ASSERTION_EQ_ERROR(__op, __lhs, __rhs, __msg_fmt, ...)         \
+  throw test::AssertionEqualityError(                                          \
+    __op,                                                                      \
+    __ASSERTION_EQ_ERROR_COMMON_ARGS(__lhs, __rhs, __msg_fmt, ## __VA_ARGS__)  \
+  )
+
+/**
+ * @brief Constructs & throws AssertionOneOfError
+ *
+ * @warning For internal use
+ *
+ * For the arguments see @ref __ASSERTION_EQ_ERROR_COMMON_ARGS
+ */
+#define __THROW_ASSERTION_ONE_OF_ERROR(__lhs, __rhs, __msg_fmt, ...)           \
+  throw test::AssertionOneOfError(                                             \
+    __ASSERTION_EQ_ERROR_COMMON_ARGS(__lhs, __rhs, __msg_fmt, ## __VA_ARGS__)  \
+  )
+
+/**
+ * @brief Perform an assertion check (is __expr true?) and throw an
+ *        AssertionError with formatted __msg_fmt if it fails
+ *
+ * @param __expr    Expression to check
+ * @param __msg_fmt std::format-compatible string for assertion failure message
+ * @param ...       Assertion failure message args
+ */
+#define TEST_ASSERT(__expr, __msg_fmt, ...)                                    \
+  do {                                                                         \
+    if (!(__expr)) {                                                           \
+      throw test::AssertionError(                                              \
+        __FILE__, __LINE__,                                                    \
+        #__expr,                                                               \
+        std::format(__msg_fmt, ## __VA_ARGS__)                                 \
+      );                                                                       \
+    }                                                                          \
   } while (0)
 
-#define TEST_ASSERT_EQ(__lhs, __rhs, __msg_fmt, ...)                          \
-  do {                                                                        \
-    if ((__lhs) != (__rhs)) {                                                 \
-      throw test::AssertionEqualityError(                                     \
-        true,                                                                 \
-        __FILE__, __LINE__,                                                   \
-        test::impl::to_string(__lhs),                                                               \
-        test::impl::to_string(__rhs),                                                               \
-        std::format(__msg_fmt, ## __VA_ARGS__)                                \
-      );                                                                      \
-    }                                                                         \
+/**
+ * @brief Perform an equality check (does __lhs == __rhs?) and throw an
+ *        AssertionEqualityError with formatted __msg_fmt if it fails
+ *
+ * @param __lhs     Left Hand Side of the equality check
+ * @param __rhs     Right Hand Side of the equality check
+ * @param __msg_fmt std::format-compatible string for assertion failure message
+ * @param ...       Assertion failure message args
+ */
+#define TEST_ASSERT_EQ(__lhs, __rhs, __msg_fmt, ...)                           \
+  do {                                                                         \
+    if ((__lhs) != (__rhs)) {                                                  \
+      __THROW_ASSERTION_EQ_ERROR(                                              \
+          true, __lhs, __rhs,                                                  \
+          __msg_fmt, ## __VA_ARGS__                                            \
+      );                                                                       \
+    }                                                                          \
   } while (0)
 
-#define TEST_ASSERT_NE(__lhs, __rhs, __msg_fmt, ...)                          \
-  do {                                                                        \
-    if ((__lhs) == (__rhs)) {                                                 \
-      throw test::AssertionEqualityError(                                     \
-        false,                                                                \
-        __FILE__, __LINE__,                                                   \
-        test::impl::to_string(__lhs),                                                               \
-        test::impl::to_string(__rhs),                                                               \
-        std::format(__msg_fmt, ## __VA_ARGS__)                                \
-      );                                                                      \
-    }                                                                         \
+/**
+ * @brief Perform a non-equality check (does __lhs != __rhs?) and throw an
+ *        AssertionEqualityError with formatted __msg_fmt if it fails
+ *
+ * @param __lhs     Left Hand Side of the non-equality check
+ * @param __rhs     Right Hand Side of the non-equality check
+ * @param __msg_fmt std::format-compatible string for assertion failure message
+ * @param ...       Assertion failure message args
+ */
+#define TEST_ASSERT_NE(__lhs, __rhs, __msg_fmt, ...)                           \
+  do {                                                                         \
+    if ((__lhs) == (__rhs)) {                                                  \
+      __THROW_ASSERTION_EQ_ERROR(                                              \
+          false, __lhs, __rhs,                                                 \
+          __msg_fmt, ## __VA_ARGS__                                            \
+      );                                                                       \
+    }                                                                          \
+  } while (0)
+
+/**
+ * @brief Test if __lhs is in one of __rhs values (__rhs contains __lhs?) and
+ *        throw an AssertionOneOfError with formatted __msg_fmt if it fails
+ *
+ * @param __lhs     LHS, Element to check
+ * @param __rhs     RHS, List of elements, where __lhs must be in
+ * @param __msg_fmt std::format-compatible string for assertion failure message
+ * @param ...       Assertion failure message args
+ */
+#define TEST_ASSERT_ONE_OF(__lhs, __rhs, __msg_fmt, ...)                       \
+  do {                                                                         \
+    bool found = false;                                                        \
+    for (const auto& element : (__rhs)) {                                      \
+      if (element == (__lhs)) {                                                \
+        found = true;                                                          \
+        break;                                                                 \
+      }                                                                        \
+    }                                                                          \
+    if (!found) {                                                              \
+      __THROW_ASSERTION_ONE_OF_ERROR(__lhs, __rhs, __msg_fmt, ## __VA_ARGS__); \
+    }                                                                          \
   } while (0)
 
 namespace test {
 
+/**
+ * @brief Basic Assertion Error
+ */
 struct AssertionError final : std::runtime_error {
   explicit AssertionError(
     const std::string& file,
@@ -97,6 +216,9 @@ struct AssertionError final : std::runtime_error {
       ) {}
 };
 
+/**
+ * @brief Equality Assertion Error
+ */
 struct AssertionEqualityError final : std::runtime_error {
   explicit AssertionEqualityError(
     bool               eq,
@@ -108,10 +230,33 @@ struct AssertionEqualityError final : std::runtime_error {
   ) : std::runtime_error(
         std::format(
           "'" ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET
-          "' {} '" ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET
+          "' {} '"
+          ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET
           " at " TEST_LOC_FMT ". Message: '"
           ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET "'",
           lhs, eq ? "!=" : "==", rhs, file, line, message
+        )
+      ) {}
+};
+
+/**
+ * @brief "Contains" or "One Of" Assertion Error
+ */
+struct AssertionOneOfError final : std::runtime_error {
+  explicit AssertionOneOfError(
+    const std::string& file,
+    int                line,
+    const std::string& lhs,
+    const std::string& rhs,
+    const std::string& message
+  ) : std::runtime_error(
+        std::format(
+          "'" ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET
+          "' was expected to be one of '"
+          ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET
+          " at " TEST_LOC_FMT ". Message: '"
+          ANSI_TEXT_BOLD "{}" ANSI_TEXT_RESET "'",
+          lhs, rhs, file, line, message
         )
       ) {}
 };
@@ -120,12 +265,18 @@ struct TestSuite;
 
 using TestFunction = std::function<void(TestSuite*)>;
 
+/**
+ * @brief Represents a single test
+ */
 struct Test {
   std::string name;
   std::string desc;
   TestFunction fn;
 };
 
+/**
+ * @brief Represents a test suite of singular tests
+ */
 struct TestSuite {
   std::string name;
 
@@ -162,8 +313,7 @@ std::string to_string(const T& val) {
       out += to_string(item);
       first = false;
     }
-    out += "]";
-    return out;
+    return out + ']';
   } else if constexpr (StandardFormattable<T>) {
     return std::format("{}", val);
   } else {
@@ -205,6 +355,14 @@ inline void reportError(Statistics& stat, Test * test, size_t name_sz, size_t de
 
 }
 
+/**
+ * @brief Run a select tests (from `to_run`) or all of them (`to_run={}`) from
+ *        a specific test suite
+ *
+ * @param suite  Test Suite to run tests from
+ * @param to_run List of tests to run (empty for all)
+ * @return Return code. All additional info will be printed into stdout
+ */
 inline int run(TestSuite * suite, std::vector<std::string> to_run = {}) {
   auto has_to_run = [&to_run](const std::string& name) {
     return std::find(to_run.begin(), to_run.end(), name) != to_run.end();
